49ded9d75747b7aad2c2543ea245ce7bf54bb701
Patrick Stephen <patrick@sandgarden.com>
2026-09-24 08:58:01 -0500

    cmd/affiro: split out signal volume sprite type

--- cmd/affiro/main.go
+++ cmd/affiro/main.go
@@ -1470,7 +1470,7 @@ func drawHistoryRow(ctx *scene.Context, sig *asigx.Asig, cols historyColumns, ne
 		c.ShiftPos(13*pixelMagnifier, 13*pixelMagnifier)
 	}
 
-	sv := NewSignalVolumeSprite(0, 0, 18*pixelMagnifier, 18*pixelMagnifier)
+	sv := oakx.NewSignalVolumeSprite(0, 0, 18*pixelMagnifier, 18*pixelMagnifier)
 	sv.SetActions(sig.TotalActions)
 
 	hoverCh := make(chan struct{})
@@ -1611,54 +1611,4 @@ func Iff[T any](darkMode bool, a, b T) T {
 		return a
 	}
 	return b
-}
-
-type signalVolumeSprite struct {
-	barColors [5]color.Color
-	*render.Sprite
-	thresholds   [4]int
-	totalActions int
-}
-
-func NewSignalVolumeSprite(x, y float64, w, h int) *signalVolumeSprite {
-	sp := render.NewEmptySprite(x, y, w, h)
-	return &signalVolumeSprite{
-		Sprite: sp,
-		thresholds: [4]int{
-			5,
-			50,
-			300,
-			1500,
-		},
-		barColors: [5]color.Color{
-			0: color.RGBA{128, 0, 0, 255},
-			1: color.RGBA{128, 128, 0, 255},
-			2: color.RGBA{0, 128, 0, 255},
-			3: color.RGBA{0, 200, 0, 255},
-			4: color.RGBA{0, 255, 0, 255},
-		},
-	}
-}
-
-func (s *signalVolumeSprite) SetActions(totalActions int) {
-	s.totalActions = totalActions
-	if s.totalActions == 0 {
-		return
-	}
-	bars := 1
-	for _, v := range s.thresholds {
-		if totalActions < v {
-			break
-		}
-		bars++
-	}
-	color := s.barColors[bars-1]
-	bds := s.GetRGBA().Bounds()
-	barXDistance := bds.Max.X / 6
-	barHeight := bds.Max.Y / 5
-	x := 1
-	for b := range bars {
-		render.DrawLine(s.GetRGBA(), x, bds.Max.Y, x, bds.Max.Y-(barHeight*b), color)
-		x += barXDistance
-	}
 }
